79e8e0f4db695c978a002e4c365f5bd42085d43c
Martín <magadiflo@gmail.com>
2022-08-04 23:54:04 -0500

    PrimeNg module

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+//* PrimeNg
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+/**
+ ** Este módulo (que agrupa los elementos de PrimeNg)
+ ** únicamente lo usaremos para exportar los módulos de PrimeNg. 
+ **
+ ** ¿Por qué también, no lo agregamos en los imports: []?, por que en este directorio
+ ** primer-ng/ correspondiente a este módulo, no tendremos ningún componente 
+ ** que use los botones, cards, o cualquier otro módulo de PrimeNg.
+ ** En caso que se cree algún componente, dentro de este directorio (prime-ng/),
+ ** y este componente requiera usar algunos de los módulos de PrimeNg, solo en ese
+ ** caso será necesario importarlo (solo el que se vaya a utilizar)
+ */
+@NgModule({
+  exports: [
+    ButtonModule,
+    CardModule,
+  ]
+})
+export class PrimeNgModule { }
