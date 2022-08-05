1b0eee3f3b7c63ca6ade61c67fc743325037b968
Martín <magadiflo@gmail.com>
2022-08-04 22:05:53 -0500

    Introducción a los Pipes de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-app';
+  
+  nombre: string = 'martín díaz flores';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Tinkler'
+  }
+
+  mostrarNombre(): void {
+    console.log(this.nombre);
+    console.log(this.valor);  
+  }
+
 }
