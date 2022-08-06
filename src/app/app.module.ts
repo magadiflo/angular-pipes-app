ac24c70b065efbfdbfd10a7b598aa71fdb742ff2
Martín <magadiflo@gmail.com>
2022-08-05 23:20:01 -0500

    Rutas de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule,
+    VentasModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
