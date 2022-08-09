d2d0634d1f93bb9e685de94bec821015848a7cd7
Martín <magadiflo@gmail.com>
2022-08-08 23:49:01 -0500

    CurrencyPipe y PercentPipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
 export class NumerosComponent {
 
   ventasNetas: number = 2567698.558966;
-  porcentaje: number = 0.48;
+  porcentaje: number = 0.4856;
 
 }
