964b481d022becd358420342bc773c808eacb282
Martín <magadiflo@gmail.com>
2022-08-09 22:39:57 -0500

    i18nPlural pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -10,12 +10,21 @@ type Genero = 'femenino' | 'masculino';
 })
 export class NoComunesComponent {
 
+  //* i18nSelect
   nombre: string = 'Martín';
   genero: Genero = 'masculino';
-
   invitacionMap = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  //* i18nPlural
+  clientes: string[] = ['María', 'Fred', 'Rosa', 'Pedro', 'Gabriel'];
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
   }
 
 }
